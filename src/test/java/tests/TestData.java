c7002420e66c86eb6924431d793fb3069faa562f
Inna Torukalo <iniska2002@gmail.com>
2025-07-07 21:13:32 +0300

    deleted RandomUtils.java

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,13 +1,10 @@
 package tests;
 
 import com.github.javafaker.Faker;
-import utils.RandomUtils;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import static utils.RandomUtils.StateWithCity;
 
 
 public class TestData {
@@ -19,17 +16,29 @@ public class TestData {
     public static String firstName = faker.name().firstName(),
             lastName = faker.name().lastName(),
             userEmail = faker.internet().emailAddress(),
-            gender = RandomUtils.getRandomGender(),
+            gender = faker.options().option("Male", "Female", "Other"),
             number = "89" + faker.number().digits(8),
-            day = (new SimpleDateFormat("dd", Locale.ENGLISH)).format(DateOfBirthday),
+            day = (new SimpleDateFormat("d", Locale.ENGLISH)).format(DateOfBirthday),
             month = (new SimpleDateFormat("MMMM", Locale.ENGLISH)).format(DateOfBirthday),
             year = (new SimpleDateFormat("y", Locale.ENGLISH)).format(DateOfBirthday),
-            subject = RandomUtils.getRandomSubject(),
-            hobbies = RandomUtils.getRandomHobby(),
+            subject = faker.options().option("Accounting", "Maths", "Arts", "English", "Physics", "Chemistry",
+                    "Computer Science", "Economics", "Social Studies", "History", "Civics",
+                    "Commerce", "Hindi", "Biology"),
+            hobbies = faker.options().option("Reading", "Sports", "Music"),
             picture = "test.jpg",
             currentAddress = faker.address().fullAddress(),
-            state = RandomUtils.getRandomState(),
-            city = faker.options().option(StateWithCity.get(state));
+            state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan"),
+            city = selectCity(state);
 
 
+    public static String selectCity(String state) {
+        return switch (state) {
+            case "NCR" -> faker.options().option("Delhi", "Gurgaon", "Noida");
+            case "Uttar Pradesh" -> faker.options().option("Agra", "Lucknow", "Merrut");
+            case "Haryana" -> faker.options().option("Karnal", "Panipat");
+            case "Rajasthan" -> faker.options().option("Jaipur", "Jaiselmer");
+            default -> null;
+        };
+
+    }
 }
